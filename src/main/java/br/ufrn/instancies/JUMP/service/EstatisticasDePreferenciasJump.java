52c173f8649a62d5cb1a1571abc665538158a8e8
RickFqt <henriquefouquet@gmail.com>
2025-01-15 15:55:23 -0300

    fix: Ajusta construcao de prompt do estatisticaLLM JUMP

--- src/main/java/br/ufrn/instancies/JUMP/service/EstatisticasDePreferenciasJump.java
+++ src/main/java/br/ufrn/instancies/JUMP/service/EstatisticasDePreferenciasJump.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.stereotype.Component;
 
 import br.ufrn.FormsFramework.model.Formulario;
+import br.ufrn.FormsFramework.model.Opcao;
 import br.ufrn.FormsFramework.model.Quesito;
 import br.ufrn.FormsFramework.model.Secao;
 import br.ufrn.FormsFramework.service.interfaces.EstatisticasLLM;
@@ -52,58 +53,41 @@ public class EstatisticasDePreferenciasJump extends EstatisticasLLM {
 
             if(secao.getTitulo().toLowerCase().equals("interesses")) {
                 
-                json.append("\t\"Nome da seção\": \"").append(secao.getTitulo()).append("\",\n");
-                json.append("\t\t\t\"quesitos\": [\n");
+                json.append("\t\"nome da seção\": \"").append(secao.getTitulo()).append("\",\n");
+                json.append("\t\"quesitos\": [\n");
 
                 for(int quesito_index = 0; quesito_index < secao.getQuesitos().size(); quesito_index++) {
                     Quesito quesito = secao.getQuesitos().get(quesito_index);
-                    json.append("\t\t\t\t{\n");
-                    json.append("\t\t\t\t\t\"nome\": \"").append(quesito.getEnunciado()).append("\",\n");
-                    json.append("\t\t\t\t\t\"respostas\": [\n");
+                    json.append("\t\t{\n");
+                    json.append("\t\t\t\"nome\": \"").append(quesito.getEnunciado()).append("\",\n");
+                    if(quesito.getOpcoes() != null) {
+                        json.append("\t\t\t\"opcoes disponíveis\": [\n");
+                        for(Opcao opcao : quesito.getOpcoes()) {
+                            json.append("\t\t\t\t\"").append(opcao.getTextoAlternativa()).append("\",\n");
+                        }
+                        json.append("\t\t\t]\n");
+                    }
+                    json.append("\t\t\t\"respostas\": [\n");
 
                     for (Formulario formulario : formularios) {
                         Secao secaoFormulario = formulario.getSecoes().get(secao_index);
                         Quesito quesitoFormulario = secaoFormulario.getQuesitos().get(quesito_index);
                         if(quesitoFormulario.getResposta() != null) {
-                            json.append("\t\t\t\t\t\t\t\"").append(quesitoFormulario.getResposta().getConteudo()).append("\",\n");
+                            json.append("\t\t\t\t\"").append(quesitoFormulario.getResposta().getConteudo()).append("\",\n");
                         } else {
-                            json.append("\t\t\t\t\t\t\t\" \",\n");
+                            json.append("\t\t\t\t\" \",\n");
                         }
                     }
 
-                    json.append("\t\t\t\t\t]\n");
-                    json.append("\t\t\t\t},\n");
+                    json.append("\t\t\t]\n");
+                    json.append("\t\t},\n");
                 }
 
-                json.append("\t\t\t]\n");
-                json.append("\t\t},\n");
+                json.append("\t]\n");
+
+                break;
             }
-            json.append("\t\t{\n");
-            json.append("\t\t\t\"nome\": \"").append(secao.getTitulo()).append("\",\n");
-            json.append("\t\t\t\"quesitos\": [\n");
-
-            for(int quesito_index = 0; quesito_index < secao.getQuesitos().size(); quesito_index++) {
-                Quesito quesito = secao.getQuesitos().get(quesito_index);
-                json.append("\t\t\t\t{\n");
-                json.append("\t\t\t\t\t\"nome\": \"").append(quesito.getEnunciado()).append("\",\n");
-                json.append("\t\t\t\t\t\"respostas\": [\n");
-
-                for (Formulario formulario : formularios) {
-                    Secao secaoFormulario = formulario.getSecoes().get(secao_index);
-                    Quesito quesitoFormulario = secaoFormulario.getQuesitos().get(quesito_index);
-                    if(quesitoFormulario.getResposta() != null) {
-                        json.append("\t\t\t\t\t\t\t\"").append(quesitoFormulario.getResposta().getConteudo()).append("\",\n");
-                    } else {
-                        json.append("\t\t\t\t\t\t\t\" \",\n");
-                    }
-                }
-
-                json.append("\t\t\t\t\t]\n");
-                json.append("\t\t\t\t},\n");
-            }
-
-            json.append("\t\t\t]\n");
-            json.append("\t\t},\n");
+            
         }
 
         json.append("}");
